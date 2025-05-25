5597d4a8af16e8b3bf58484be5803ebd7839148d
Wendell Bassi <wendell.mendes@industrycare.com.br>
2025-05-25 10:58:51 -0300

    Fix import errors

--- grafo_api/grafo_backend/__init__.py
+++ grafo_api/grafo_backend/__init__.py
@@ -2,7 +2,7 @@
 Módulo de inicialização do backend para estudo de teoria dos grafos.
 """
 
-from core import Grafo, Vertice, Aresta
+from .core import Grafo, Vertice, Aresta
 
 __version__ = '0.1.0'
 __all__ = ['Grafo', 'Vertice', 'Aresta']
