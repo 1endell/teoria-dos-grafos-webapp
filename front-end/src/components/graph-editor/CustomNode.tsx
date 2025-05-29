7e3347426150deebf2729af4a8fd7c76411866e1
Wendell Bassi <wendell.mendes@industrycare.com.br>
2025-05-29 14:02:19 -0300

    Fix errors

--- front-end/src/components/graph-editor/CustomNode.tsx
+++ front-end/src/components/graph-editor/CustomNode.tsx
@@ -1,53 +1,20 @@
-import { Handle } from '@xyflow/react';
-
 export default function CustomNode({ id }) {
-  const size = 60 * 0.4; // 40% do tamanho original
-
   return (
     <div
       style={{
         background: '#4f46e5',
         color: 'white',
         borderRadius: '50%',
-        width: size,
-        height: size,
+        width: 24, // Tamanho reduzido (40%)
+        height: 24,
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         fontWeight: 'bold',
-        position: 'relative',
+        cursor: 'move',
       }}
     >
       {id}
-      <Handle
-        type="source"
-        position="top"
-        style={{
-          width: size,
-          height: size,
-          position: 'absolute',
-          top: '50%',
-          left: '50%',
-          transform: 'translate(-50%, -50%)',
-          borderRadius: '50%',
-          opacity: 0,
-          cursor: 'crosshair',
-        }}
-      />
-      <Handle
-        type="target"
-        position="top"
-        style={{
-          width: size,
-          height: size,
-          position: 'absolute',
-          top: '50%',
-          left: '50%',
-          transform: 'translate(-50%, -50%)',
-          borderRadius: '50%',
-          opacity: 0,
-        }}
-      />
     </div>
   );
 }
